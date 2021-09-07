ea2308c1b4b45de9a4e29b7481baf64d1e1492a7
Stênio Amorim <61470896+stamorim28@users.noreply.github.com>
2021-09-07 10:41:49 -0300

    alt_geracao

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0'
+    baseURL: 'https://pokeapi.co/api/v2/pokemon?limit=233&offset=151'
 });
 
 export default api;
