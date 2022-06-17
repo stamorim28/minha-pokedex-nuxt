28bb28d4b469f7abdb8c29b6510a19cf06013415
Stenio <jstenio.rocha@gmail.com>
2022-06-17 20:41:19 -0300

    feat: new base's config of project

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,6 +46,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxt/typescript-build'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
