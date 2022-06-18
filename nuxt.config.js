d4ad176e40e6c55179cf9bfe3778e17a27cf2f2b
Stenio <jstenio.rocha@gmail.com>
2022-06-18 10:07:54 -0300

    feat: baseURL handling endpoint

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,9 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: 'https://pokeapi.co/api/v2/pokemon'
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
