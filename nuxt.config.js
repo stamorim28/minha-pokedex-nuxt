f9d74ad493e9bfd08e4012d0c9cd09a4a045972a
Stenio <jstenio.rocha@gmail.com>
2022-06-17 22:20:06 -0300

    refac: index.scss new global css

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,10 +32,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    './assets/css/reset.css',
-    './assets/css/poketypes.css'
-  ],
+  css: ["@/assets/scss/index.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
